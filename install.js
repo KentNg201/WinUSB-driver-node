f9edefd1f28f9c561c8e2c1ebad55cb843ef3c5b
Kent Ng <kent.ng@newtons.tech>
2019-03-05 12:06:43 +0800

    complete driver installer module

--- install.js
+++ install.js
@@ -1,9 +1,25 @@
 var exec = require('child_process').execFile;
 var bat = 'cmd.exe';
-var param = ["/c", "start", "\"\"", "./driver/wdi-simple.exe", "--type", "0", "--name",  Math.random().toString(36).substring(7), "--vid", process.argv[2], "--pid", process.argv[3]];
+var param = ['/c', 'start', '""', './driver/wdi-simple.exe', '--type', '0', '--name', Math.random().toString(36).substring(7), '--vid', process.argv[2], '--pid', process.argv[3]];
+var fs = require('fs');
 
 if (process.argv.length >= 4) {
+  fs.writeFile("--type", '', (err) => {
+    if (err) throw err;
+  });
+
   exec(bat, param, () => {});
+
+  fs.watch("--type", () => {
+    var data = fs.readFile("--type", {encoding: 'utf8'}, (err, data) => {
+      if (data != '') {
+        if (data.includes("success")) {
+          console.log(data);
+          fs.unlink("--type");
+        } 
+      }
+    });
+  });
 } else {
   console.log("Missing vid or pid");
 }
--- print.js
+++ print.js
@@ -1,5 +1,6 @@
 const escpos = require('escpos');
 const device  = new escpos.USB(0x1FC9, 0x2016);
+// const device  = new escpos.USB(0x0471, 0x0055);
 const options = { encoding: "GB18030" /* default */ };
 const printer = new escpos.Printer(device, options);
 
@@ -11,10 +12,9 @@ device.open(() => {
   .size(1, 1)
   .text('The quick brown fox jumps over the lazy dog')
   .text('敏捷的棕色狐狸跳过懒狗')
-  // .barcode('1234567', 'EAN8')
-  // .qrimage('https://github.com/song940/node-escpos', (err) => {
-    
-  // });
-  printer.cut();
-  printer.close();
+  .barcode('1234567', 'EAN8')
+  .qrimage('https://github.com/song940/node-escpos', (err) => {
+    printer.cut();
+    printer.close();
+  });
 });
